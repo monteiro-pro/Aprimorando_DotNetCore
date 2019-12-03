93bf5fa1e763a2aaf2002cc644a56c4d1ffe9413
Jose Monteiro da Silva <j.monteiro.da.silva@avanade.com>
2019-12-03 00:25:47 -0300

    .gitignore is now working

---
+++ Aprimorando_DotNetCore/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Aprimorando.Lib;
+
+namespace Aprimorando_DotNetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var jogador1 = new Jogador1("Ronaldo");
+            var jogador2 = new Jogador1("Bebeto");
+
+            JogoFoda.Acao(jogador1);
+            JogoFoda.Acao(jogador2);
+        }
+    }
+}
